14f972403a10ae4ab4a665f5e53add20066772b7
ravenbbs <julioc3bgud@gmail.com>
2024-09-08 12:31:32 -0500

    add routes on app

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ContextProvider } from "../src/Components/utils/global.context";
+import { BrowserRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ContextProvider>
-    <App/>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
     </ContextProvider>
   </React.StrictMode>
 );
