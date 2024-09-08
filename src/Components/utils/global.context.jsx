324e6ae35711572afe316c8051629ee9cbb6e554
ravenbbs <julioc3bgud@gmail.com>
2024-09-08 12:31:14 -0500

    add api function

--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -1,38 +1,55 @@
-import { createContext, useEffect, useMemo, useReducer } from "react";
+  import { createContext, useEffect, useMemo, useReducer } from "react";
 
-export const initialState = {
-  theme: localStorage.getItem("theme") || "nord", // Tema inicial
-  data: [], // Datos de la API
-};
+  export const initialState = {
+    theme: localStorage.getItem("theme") || "nord", // Tema inicial
+    data: [], // Datos de la API
+    favs: JSON.parse(localStorage.getItem("favs")) || [], // Lista de dentistas favoritos
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "SET_THEME":
-      localStorage.setItem("theme", action.payload);
-      return { ...state, theme: action.payload };
-    case "SET_DATA":
-      return { ...state, data: action.payload };
-    default:
-      return state;
-  }
-};
+  };
 
-export const ContextGlobal = createContext(undefined);
 
-export const ContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "SET_THEME":
+        localStorage.setItem("theme", action.payload);
+        return { ...state, theme: action.payload };
+      case "SET_DATA":
+        return { ...state, data: action.payload };
+      case "ADD_TO_FAVS":
+        const updatedFavs = [...state.favs, action.payload];
+        localStorage.setItem("favs", JSON.stringify(updatedFavs));
 
-  useEffect(() => {
-    document.documentElement.setAttribute("data-theme", state.theme);
-  }, [state.theme]);
+        return { ...state, favs: updatedFavs };
+      default:
+        return state;
+    }
+  };
 
-  const contextValue = useMemo(() => {
-    return { state, dispatch };
-  }, [state]);
+  export const ContextGlobal = createContext(undefined);
 
-  return (
-    <ContextGlobal.Provider value={contextValue}>
-      {children}
-    </ContextGlobal.Provider>
-  );
-};
+  export const ContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+      document.documentElement.setAttribute("data-theme", state.theme);
+    }, [state.theme]);
+
+    useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data => {
+        dispatch({ type: "SET_DATA", payload: data });
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+    const contextValue = useMemo(() => {
+      return { state, dispatch };
+    }, [state]);
+
+    return (
+      <ContextGlobal.Provider value={contextValue}>
+        {children}
+      </ContextGlobal.Provider>
+    );
+  };
