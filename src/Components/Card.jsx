1d034348f3a0d4ae5e3da21ec9260304afa60405
ravenbbs <julioc3bgud@gmail.com>
2024-09-09 10:59:27 -0500

    ad heart button and animation

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,20 +1,42 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { ContextGlobal } from "./utils/global.context";
+import { Link } from "react-router-dom";
+import "../index.css";
+import HeartButton from "./HeartButton";
 
 const Card = ({ name, username, id }) => {
+  const { dispatch } = useContext(ContextGlobal);
 
-  const addFav = ()=>{
-    // Aqui iria la logica para agregar la Card en el localStorage
-  }
+  const addFav = () => {
+    // Crear un objeto con la información del dentista
+    const dentist = { id, name, username };
 
+    // Llamar al dispatch para agregarlo a favoritos
+    dispatch({ type: "ADD_TO_FAVS", payload: dentist });
+  };
   return (
     <div className="card">
-        {/* En cada card deberan mostrar en name - username y el id */}
-
-        {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
-
-        {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-        <button onClick={addFav} className="favButton">Add fav</button>
+      <div className="card bg-base-100 w-96 shadow-xl">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          {/* Muestra el name, username y el id */}
+          <h2 className="card-title">{name}</h2>
+          <p>{username}</p>
+          <div className="card-actions justify-end">
+            {/* Link para navegar al detalle del dentista */}
+            <Link to={`/dentist/${id}`} className="btn btn-primary">
+              Ver detalles
+            </Link>
+            {/* Botón para agregar a favoritos */}
+            <HeartButton patientId={id} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
